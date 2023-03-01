a2634d3917c6dc68b65e18372d9ccd440c68869b
Joshua Stuart <Joshuasstuart@gmail.com>
2023-03-01 14:14:04 -0500

    fixed/removed Mongoose errors

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
 });
 
-app.post("/", function(req, res){
+const itemsSchema = new Schema({
+  _id: Number,
+  name: String,
+});
 
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welcome to the ToDo List 😩",
+});
+const item2 = new Item({
+  name: "Click + button to add new item.",
+});
+const item3 = new Item({
+  name: "Click - this to delete an item.",
+});
+
+const defaultItems = [item1, item2, item3];
+// Item.insertMany(defaultItems, function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved default items to database.");
+//   }
+// });
+Item.insertMany(defaultItems);
+
+const workItems = [];
+
+app.get("/", function (req, res) {
+  res.render("list", { listTitle: "Today", newListItems: items });
+});
+
+app.post("/", function (req, res) {
   const item = req.body.newItem;
 
   if (req.body.list === "Work") {
@@ -34,14 +58,14 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
